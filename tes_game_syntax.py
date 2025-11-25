13dddedc66fc2b698ed281d283ce1f2ed126f8a2
Jonas <uspqk@student.kit.edu>
2025-11-25 14:05:38 +0100

    add project files

--- tes_game_syntax.py
+++ tes_game_syntax.py
@@ -1,9 +1,8 @@
-from Results.results import Result, ResultType
+from results.results import Result
 from Commands.commands import BidCommand, TradeCommand, ShowStatsCommand
 
 from Inputs.inputs import ConsoleInputHandler
 from Game.game import Game
-
 
 
 if __name__ == "__main__":
@@ -12,11 +11,12 @@ if __name__ == "__main__":
     game = Game()
     game.start_game()
 
-    
-    dict = {'cow_amount' : 1,
-            'player_who_gets_cow' : 0,
-            'player_who_gets_money' : 1,
-            'money_amount' : [0,0,1,0,0,0]}
+    dict = {
+        "cow_amount": 1,
+        "player_who_gets_cow": 0,
+        "player_who_gets_money": 1,
+        "money_amount": [0, 0, 1, 0, 0, 0],
+    }
 
     command = BidCommand()
     param = inptHandler.get_inputs_for_command(command)
@@ -24,5 +24,3 @@ if __name__ == "__main__":
     command.fill(param)
     res = command.validate_command_values(game)
     print(res)
-
-    
